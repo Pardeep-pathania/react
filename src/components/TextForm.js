854f82350b9ac4ea3b0020eb700f433d81b37266
pardeep-pathania <pathanianikhil145@gmail.com>
2025-01-12 15:48:54 +0530

    add clear text button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,8 +2,8 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
 
-    const handleUpClick =()=>{
-        console.log("Uppercase was clicked"+ text);
+    function handleUpClick() {
+        console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }  
@@ -11,6 +11,10 @@ export default function TextForm(props) {
     const handleLoClick =()=>{
         console.log("Lowercase was clicked"+ text);
         let newText = text.toLowerCase();
+        setText(newText);
+    }  
+    const handleClearClick =()=>{
+        let newText = ("");
         setText(newText);
     }  
 
@@ -28,9 +32,10 @@ export default function TextForm(props) {
 <div className="mb-3">
   <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
 </div>
-<button className="btn btn-primary mx-2" onClick={handleUpClick}>convert to uppercase</button>
-<button className="btn btn-primary mx-2" onClick={handleLoClick}>convert to uppercase</button>
-    </div>
+<button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
+<button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase</button>
+<button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+</div>
 
     <div className="container my-3">
         <h2>Your Text summary</h2>
@@ -39,7 +44,8 @@ export default function TextForm(props) {
         <h3>Preview</h3>
         <p>{text}</p>
     </div>
-    </> 
+    </>
+    
+  ) 
   
-  )
 }
