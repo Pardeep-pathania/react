bfc5d1d8b4cb728d9220189f2b3b95518f1747d6
pardeep-pathania <pathanianikhil145@gmail.com>
2025-01-13 19:21:14 +0530

    add copy text button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,13 +3,13 @@ import React, {useState} from 'react'
 export default function TextForm(props) {
 
     function handleUpClick() {
-        console.log("Uppercase was clicked" + text);
+       // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
     }  
 
     const handleLoClick =()=>{
-        console.log("Lowercase was clicked"+ text);
+        //console.log("Lowercase was clicked"+ text);
         let newText = text.toLowerCase();
         setText(newText);
     }  
@@ -19,9 +19,16 @@ export default function TextForm(props) {
     }  
 
     const handleOnChange =(event)=>{
-        console.log("On change");
+        //console.log("On change");
         setText(event.target.value);
     } 
+
+    const handleCopy = () => {
+      //console.log("Text Copy")
+      var text = document.getElementById("myBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
 
     const [text, setText] = useState("");
 
@@ -35,6 +42,7 @@ export default function TextForm(props) {
 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase</button>
 <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+<button className="btn btn-primary mx-2" onClick={handleCopy}>Copy text</button>
 </div>
 
     <div className="container my-3">
